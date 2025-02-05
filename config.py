89c2eafd509a8a5b314b4511627526fbd0d60309
AEBC08 <125038182+AEBC08@users.noreply.github.com>
2025-02-05 12:43:56 +0800

    Delete config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-# DeepSeek API 配置
-DEEPSEEK_API_URL = "https://api.siliconflow.cn/v1/chat/completions"
-DEEPSEEK_API_KEY = 'sk-'
-MODEL = 'deepseek-ai/DeepSeek-V3'
-#单次回复最大token
-MAX_TOKEN = 2000
-#温度
-TEMPERATURE = 1.2
